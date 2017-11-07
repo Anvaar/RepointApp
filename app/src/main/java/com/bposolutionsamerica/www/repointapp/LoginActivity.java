f9a8f54ea11dc8e63725eb289848b65ef2d82159
Anvaar <Anvaar@users.noreply.github.com>
2017-11-07 21:01:57 +0500

    Initial commit

---
+++ app/src/main/java/com/bposolutionsamerica/www/repointapp/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.bposolutionsamerica.www.repointapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button btn1 = (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginActivity.this,
+                        WelcomeActivity.class);
+                startActivity(myIntent);
+                //your stuff here.
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
+
+}
