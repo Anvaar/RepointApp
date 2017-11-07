f5027292046ee574625740d07a457872b88586c2
Anvaar <Anvaar@users.noreply.github.com>
2017-11-07 21:08:34 +0500

    Update files

---
+++ app/src/main/java/com/bposolutionsamerica/www/repointapp/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.bposolutionsamerica.www.repointapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        TextView btn1 = (TextView)findViewById(R.id.button1);
+
+    }
+}
